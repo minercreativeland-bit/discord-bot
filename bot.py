57c14d39e58c14d6baaaf92d563ad6d1a506241f
minercreativeland-bit <minercreativeland@gmail.com>
2026-09-05 15:59:08 -0500

    Integrate Flask server for bot uptime
    Added a Flask web server to keep the bot online 24/7.

--- bot.py
+++ bot.py
@@ -1,25 +1,43 @@
+import json
 import os
+import random
+from threading import Thread
 import discord
 from dotenv import load_dotenv
+from flask import Flask
+
+# --- Tiny web server to keep Render Web Service happy ---
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Bot is online and running 24/7!'
+
+
+def run_web():
+  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+
+def keep_alive():
+  t = Thread(target=run_web)
+  t.start()
+
+
+# --------------------------------------------------------
 
 load_dotenv()
-TOKEN = os.getenv('MTU0NTYzNjEyNzA5NDg2NTkzMA.Gpdnu4.zu6sXmsCwyr94CCZ0OIkpSaSlK9HW8ZQTY2cNY')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'Success! Logged in as {client.user}')
+DATA_FILE = 'streaks.json'
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# ... (keep all your existing load_data, save_data, and command code here) ...
 
-    if message.content == '!ping':
-        await message.channel.send('Pong!')
-
-client.run(TOKEN)
+if __name__ == '__main__':
+  keep_alive()  # Starts the web server in the background
+  client.run(TOKEN)  # Starts your Discord bot
